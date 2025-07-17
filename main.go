78c04a51eb204871e4d03c65f502c0c53c2751d3
Pavel <pavel.oliynyk@gmail.com>
2025-07-17 14:35:41 +0100

    feat: create persons records producer and cosumer

--- main.go
+++ main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"google.golang.org/protobuf/proto"
-	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
 	pb "protoBufPlay/github.com/protocolbuffers/protobuf/examples/go/tutorialpb"
 )
 
 func main() {
 
-	person := &pb.Person{
-		Id:    1234,
-		Name:  "John Doe",
-		Email: "jdoe@example.com",
-		Phones: []*pb.Person_PhoneNumber{
-			{Number: "555-4321", Type: pb.Person_MOBILE},
-		},
+	// Create an iterator to produce 10M person records
+	personItemProducer := func() <-chan pb.Person {
+		const maxPersonsToBeCreated int = 10000000
+		const bufferSize int = 1024
+		results := make(chan pb.Person, bufferSize)
+		// Some processing function
+		go func() {
+			defer close(results)
+			// TODO: add random data
+			person := pb.Person{
+				Id:    rand.Int31n(10000000),
+				Name:  "John Doe",
+				Email: "jdoe@example.com",
+				Phones: []*pb.Person_PhoneNumber{
+					{Number: "555-4321", Type: pb.Person_MOBILE},
+				},
+			}
+			for i := 0; i < maxPersonsToBeCreated; i++ {
+				results <- person
+			}
+		}()
+		return results
 	}
 
-	fmt.Printf("Person, %s!\n", person)
-
-	out, _ := proto.Marshal(person)
-
-	if err := ioutil.WriteFile("/Users/pavel/devcore/GoLang/protoBufPlay/data/person.dat", out, 0644); err != nil {
-		log.Fatalln("Failed to write address book:", err)
+	// consume persons records from a readonly channel: write all to a fille
+	// TODO: implement batch writing
+	personConsumer := func(persons <-chan pb.Person) {
+		const fileName string = "/Users/pavel/devcore/GoLang/protoBufPlay/data/person.dat"
+		file, _ := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		for nextPerson := range persons {
+			personInBytes, _ := proto.Marshal(&nextPerson)
+			_, err := file.Write(personInBytes)
+			//err := os.WriteFile(fileName, personInBytes, 0644)
+			if err != nil {
+				log.Fatalln("Failed to write address book:", err)
+			}
+		}
 	}
 
+	// MAIN LOGIC
+	persons := personItemProducer()
+	personConsumer(persons)
 }
